ef782a866b6a5639867aaf17549d2a941b51c3bc
Wood, Bryan Michael <woodbrmi@iu.edu>
2023-04-14 14:32:42 -0400

    Initial commit

---
+++ src/test/java/c322/assetManagement/configurationserver/ConfigurationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package c322.assetManagement.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
